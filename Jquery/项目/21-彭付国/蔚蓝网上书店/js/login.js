6e2b4b8718bca8b77ce939462e1b02b78c22356d
w956981889 <1096144240@qq.com>
2018-10-22 13:52:32 +0800

    彭付国提交作业

---
+++ Jquery/项目/21-彭付国/蔚蓝网上书店/js/login.js
@@ -0,0 +1,37 @@
+$(function($){
+	$("#email").focus(function(){
+		$(this).removeClass().addClass("login_content_input login_content_input_Focus");
+	}).blur(function(){
+		$(this).removeClass().addClass("login_content_input");
+	})
+	$("#pwd").focus(function(){
+		$(this).removeClass().addClass("login_content_input login_content_input_Focus");
+	}).blur(function(){
+		$(this).removeClass().addClass("login_content_input");
+	});
+	$("#loginForm").submit(function(){
+		var email=$("#email").val();
+		var pwd=$("#pwd").val();
+		if(email==""){
+			alert("请输入Email地址或昵称");
+			return false;
+		}
+		if(pwd==""){
+			alert("请输入密码");
+			return false;
+		}
+		return true;
+	});
+	$("#btn").mouseover(function(){
+		$(this).removeClass().addClass("login_btn_over");
+	}).mouseout(function(){
+		$(this).removeClass().addClass("login_btn_out");
+	});
+	$("#quick_register").mouseover(function(){
+		$(this).removeClass().addClass("login_register_over");
+	}).mouseout(function(){
+		$(this).removeClass().addClass("login_register_out");
+	}).click(function(){
+		window.location.href="register.html";
+	});
+});
